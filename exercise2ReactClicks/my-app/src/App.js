8d930087ac915e994ba6751f93dbe97bbd79b7b2
Oscar Hero <thescer6@gmail.com>
2022-05-20 12:43:05 +0700

    reformatted button components to recieve onclick as a prop

--- exercise2ReactClicks/my-app/src/App.js
+++ exercise2ReactClicks/my-app/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import { ButtonHolder } from "./components/ButtonHolder";
 import ClickButton from "./components/ClickButton";
 
 function App() {
   return (
     <div>
-      <ClickButton name="button 1" children="button 1"></ClickButton>
-      <ClickButton name="button 2" children="button 2"></ClickButton>
-      <ClickButton name="button 3" children="button 3"></ClickButton>
+      <ButtonHolder name="button 1" children="button 1" />
+      <ButtonHolder name="button 2" children="button 2" />
+      <ButtonHolder name="button 3" children="button 3" />
     </div>
   );
 }
---
+++ exercise2ReactClicks/my-app/src/components/ButtonHolder.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ClickButton from "./ClickButton";
+
+export const ButtonHolder = (props) => {
+  function clickHandler() {
+    const alertStr = "you clicked " + props.name;
+    console.log(props.name);
+    alert(alertStr);
+  }
+
+  return <ClickButton onClick={clickHandler} children={props.children} />;
+};
